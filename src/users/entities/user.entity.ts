4d4c23589785f41423996cd0a7a103e708d7d266
Jose Gabriel <josripe19@gmail.com>
2021-01-23 23:00:14 -0500

    completed users CRUD

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,6 +2,12 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'id unique identifier' })
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field(() => Int)
+  age: number;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  users: User[] = [
+    { id: 1, age: 20, name: 'Jose Rios' },
+    { id: 2, age: 21, name: 'Pepe Prieto' },
+  ];
+
   create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+    const maxId = Math.max(...this.users.map((user) => user.id));
+    const newUser: User = { id: maxId + 1, ...createUserInput };
+    this.users.push(newUser);
+    return newUser;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(): User[] {
+    return this.users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): User {
+    return this.users.find((user) => user.id === id);
   }
 
   update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+    const index = this.users.findIndex((user) => user.id === id);
+    this.users[index] = { ...this.users[index], ...updateUserInput };
+    return updateUserInput;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const deleted = this.users.find((user) => user.id === id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return deleted;
   }
 }
