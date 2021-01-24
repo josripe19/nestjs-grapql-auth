c81090b6c7b4dc013f29b0a672bac3362638380c
Jose Gabriel <josripe19@gmail.com>
2021-01-23 21:51:44 -0500

    added user resource with the nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { AuthorsResolver } from './authors/authors.resolver';
 import { AuthorsService } from './authors/services/authors.service';
 import { PostsResolver } from './authors/posts.resover';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, AuthorsResolver, PostsResolver, AuthorsService],
