0ed26dbbd172b0684974594cd66d5a44ed548188
Jose Gabriel <josripe19@gmail.com>
2021-01-27 02:37:02 -0500

    JwtAuthGuard implemented

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategies/jwt-strategy';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'secret-key',
+      secret: jwtConstants.secret,
     }),
   ],
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, JwtStrategy],
 })
 export class UsersModule {}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { LoginUserInput } from './dto/login-user.input';
 import { AuthPayload } from './dto/auth-payload.dto';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -21,6 +23,7 @@ export class UsersResolver {
   }
 
   @Query(() => [User], { name: 'users' })
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
@@ -28,6 +31,12 @@ export class UsersResolver {
   @Query(() => User, { name: 'user' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.usersService.findOne(id);
+  }
+
+  @Query(() => User, { name: 'me' })
+  @UseGuards(JwtAuthGuard)
+  currentProfile(@Context('req') { user }: any) {
+    return this.usersService.findOne(user.id);
   }
 
   @Mutation(() => User)
